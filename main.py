8742f493c0584587c0096f2715a302087e3e36bd
emininmac <mamihaha4@gmail.com>
2022-10-14 15:39:02 +0300

    changes

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ from forms import CreatePostForm, CreateNewUser, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 # wrap function yarattık
 from functools import wraps
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -52,7 +53,7 @@ def admin_only(f):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
